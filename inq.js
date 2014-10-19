bbba12596c9c41ada9f315d90d4cdb4acbbd07c1
hustcer <hustcer@gmail.com>
2014-10-19 18:27:59 +0800

    Add bluebird and option parsing support

--- inq.js
+++ inq.js
@@ -6,9 +6,10 @@ var pkg = require('./package.json');
 
 var colors  = require('colors');
 var printf  = require('printf');
-var program = require('commander');
+var cmd     = require('commander');
+var query   = require('./lib/index.js');
 
-program
+cmd
   .version('0.1.0')
   .option('-s, --symbol  <symbols>', 'Symbol list to query, separated by comma.')
   .option('-t, --time    <span>'   , 'Insider trading time span, should be one of:1m, 2m, 3m, 4m, or 5m.')
@@ -23,3 +24,34 @@ program
     console.log('');
   })
   .parse(process.argv);
+
+var month, symbols;
+
+if(cmd.time){
+    month = parseInt(cmd.time);
+    month = month < 0 ? 0:month;
+    month = month > 5 ? 5:month;
+}else{
+    // Query insider tradings of latest 3 months by default.
+    month = 3;
+}
+
+if(cmd.symbol){
+    symbols = cmd.symbol.split(',');
+}
+
+if(cmd.args.length > 0 ){
+    if(cmd.args.length === 1 && cmd.args[0].indexOf(',') > 0){
+        symbols = cmd.args[0].split(',');
+    }else{
+        symbols = cmd.args;
+    }
+}
+
+if(!symbols){
+    console.error('\nPlease input the symbol to query. For example: `inq aapl`\n'.red);
+    return false;
+}
+
+query.queryInsiderOf(symbols, month);
+
